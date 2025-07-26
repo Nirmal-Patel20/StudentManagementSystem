6df02e6cb1833e2b7fa305612da33a3150efb7ad
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-27 02:01:42 +0530

    did some check whether program run perfect or not

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,17 @@
 
 int main() {
 
-    std::cout << "hello world" << std::endl;
+    StudentsManager system("DataStore.txt");
+
+    /*
+    system.addStudent(Student("Nirmal",2012,19,3.9));
+    system.addStudent(Student("Shruti",7778,22,3));
+    system.addStudent(Student("Janvi",2312,20,3.8));
+    */
+
+    system.removeStudentById(7778);
+
+    system.displayALL();
     
     return 0;
 }
