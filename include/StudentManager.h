17a57159a0b7625d722c7d95f0665095d044cf2a
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-27 14:24:40 +0530

    changed name to StudentManager.H

---
+++ include/StudentManager.h
@@ -0,0 +1,34 @@
+#ifndef STUDENTMANAGER_H
+#define STUDENTMANAGER_H
+
+#include "Student.h"
+#include <vector>
+#include <fstream>
+
+class StudentManager {
+    std::vector<Student>Students;
+    std::string m_filename;
+
+    void load_file ();
+    
+    void save_file ();
+
+public : 
+
+    StudentManager (const std::string& filename) : m_filename(filename) {
+        load_file();
+    }
+
+    ~StudentManager (){
+        save_file();
+    }
+
+    void addStudent (const Student& src);
+
+    void removeStudentById (int Student_ID);
+
+    void displayALL();
+};
+
+
+#endif
