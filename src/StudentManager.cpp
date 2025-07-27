5b68f3122f74271bdd433f275a5f1456a79d90fb
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-27 14:25:21 +0530

    changed name to StudentManager.cpp

---
+++ src/StudentManager.cpp
@@ -0,0 +1,47 @@
+#include "StudentManager.h"
+
+void StudentManager::load_file (){
+
+    Student S1;
+    std::ifstream infile(m_filename);
+
+    if(infile.is_open()){
+        while(infile >> S1){
+        Students.push_back(S1);
+        }
+    }else{
+        std::cout << "fail to open file : " << m_filename << std::endl;
+    }
+}
+
+void StudentManager::save_file(){
+
+    std::ofstream outfile(m_filename);
+
+    if(outfile.is_open()){
+        for(const Student src : Students){
+            outfile << src << std::endl;
+        }
+    }else{
+        std::cout << "fail to open file : " << m_filename << std::endl;
+    }
+}
+
+void StudentManager::addStudent(const Student& src){
+    Students.push_back(src);
+}
+
+void StudentManager::removeStudentById(int Student_ID){
+    for(auto it = Students.begin(); it != Students.end();++it){
+        if(it->get_Student_ID() == Student_ID){
+            Students.erase(it);
+            break;
+        }
+    }
+}
+
+void StudentManager::displayALL() {
+    for(const Student& src : Students){
+        std::cout << src << std::endl;
+    }
+}
