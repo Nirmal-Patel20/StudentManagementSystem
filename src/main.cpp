053ba1bd1cbd9b6bb593f523ff8a9dafde056d6e
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-27 18:24:07 +0530

    did some experiment

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@ int main() {
 
     StudentManager system("DataStore.txt");
 
-    /*
-    system.addStudent(Student("Nirmal",2012,19,3.9));
+    
+    system.addStudent(Student("Nirmal",2012,19,3.9)); //Student(name,Student_ID,age,GPA)
     system.addStudent(Student("Shruti",7778,22,3));
     system.addStudent(Student("Janvi",2312,20,3.8));
-    */
+    
 
-    system.removeStudentById(7778);
+    system.removeStudentById(2002); //remove Student with ID 2002
 
-    system.displayALL();
+    system.displayALL(); //display the existence Students
     
     return 0;
 }
