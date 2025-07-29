78205ec549c0372779d5af595f69b9e59857d7de
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-29 15:41:57 +0530

    added more member var and overload of '=='

--- include/Student.h
+++ include/Student.h
@@ -7,23 +7,33 @@
 
 class Student {
     std::string m_name{"none"};
-    int m_age {0};
     int m_student_ID {0};
+    int m_age {0};
+    std::string m_email{"not given"};
+    int m_college_year {0};
     float m_GPA {0};
 
 public : 
     Student () = default;
-    Student (std::string_view name,int student_ID,int age,float GPA);
+    Student (std::string_view name,int student_ID,int age);
 
     friend std::ostream& operator<< (std::ostream& os,const Student& src);
 
     friend std::istream& operator>> (std::istream& is,Student& src);
 
-    //getter
-    std::string get_name() const;
-    int get_Student_ID () const;
+    bool operator== (const Student& src);
 
-    void display() const;
+    //getters
+    std::string get_name() const;
+    int get_Student_ID() const;
+    int get_age() const;
+
+    //setters
+    void set_extra_info(std::string_view email ,int college_year, float GPA);
+
+    void display () const;
+
+    void display_detaild_info() const;
 };
 
 #endif
--- src/Student.cpp
+++ src/Student.cpp
@@ -1,12 +1,13 @@
 #include "Student.h"
 
-Student::Student (std::string_view name,int student_ID,int age,float GPA) : m_name(name), m_student_ID(student_ID),
-                    m_age(age), m_GPA(GPA) {
+Student::Student (std::string_view name,int student_ID,int age) : m_name(name), m_student_ID(student_ID),
+                    m_age(age) {
 
 }
 
 std::ostream& operator<< (std::ostream& os,const Student& src){
-    os << src.m_name << ","  << src.m_student_ID << ","  << src.m_age << "," << src.m_GPA << ".";
+    os << src.m_name << ","  << src.m_student_ID << ","  << src.m_age << "," << src.m_email << "," 
+            << src.m_college_year << "," << src.m_GPA << ".";
     return os;
 }
 
@@ -25,12 +26,25 @@ std::istream& operator>> (std::istream& is,Student& src){
         std::getline(iss,stringTo,',');
         src.m_age = std::stoi(stringTo);
 
+        std::getline(iss,src.m_email,',');
+
+        std::getline(iss,stringTo,',');
+        src.m_college_year = std::stoi(stringTo);
+
         std::getline(iss,stringTo,'.');
         src.m_GPA = std::stof(stringTo);
     }
 
     return is;
 
+}
+
+bool Student::operator== (const Student& src){
+    if(m_name == src.m_name && m_student_ID == src.m_student_ID){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 std::string Student::get_name() const {
@@ -41,7 +55,22 @@ int Student::get_Student_ID () const {
     return m_student_ID;
 }
 
- void Student::display() const {
+int Student::get_age () const {
+    return m_age;
+}
+
+void Student::set_extra_info(std::string_view email ,int college_year, float GPA){
+    m_email = email;
+    m_college_year = college_year;
+    m_GPA = GPA;
+}
+
+void Student::display() const {
     std::cout << "Name : " << m_name << ", Student_ID : " << m_student_ID << ", Age : " 
                     << m_age << ", GPA : " << m_GPA << std::endl;
- }
+}
+
+void Student::display_detaild_info () const {
+    display();
+    std::cout << "email_ID : " << m_email << ", College year : " << m_college_year << ", GPA : " << m_GPA << std::endl;
+}
