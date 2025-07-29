f188a976b17aadec2686fde7ade360fae18b8766
Nirmal-Patel20 <nirmalpatel1206@gmail.com>
2025-07-29 14:53:34 +0530

    move template defination to header file

--- include/inputhandler.h
+++ include/inputhandler.h
@@ -8,6 +8,15 @@ bool isOnlynumber (const std::string& src);
 
 template <typename T> bool getvalidinput (T& input);
 
-bool getvalidGetlineString (std::string& input);
+template <typename T> bool getvalidinput (T& input) { //template defination must be in header
+    std::cin >> input;
+    if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return false;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return true;
+}
 
 #endif
--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -4,17 +4,6 @@ bool isOnlynumber (const std::string& src){
     return !src.empty() && std::all_of(src.begin(),src.end(), ::isdigit);
 }
 
-template <typename T> bool getvalidinput (T& input) {
-    std::cin >> input;
-    if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        return false;
-    }
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    return true;
-}
-
 bool getvalidGetlineString (std::string& input){
     std::getline(std::cin,input);
     if(std::cin.fail()){
